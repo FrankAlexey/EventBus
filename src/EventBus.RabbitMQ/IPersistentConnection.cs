2c12039d78379992cab6b30b7ba9698970054ce7
FrankAlexey <frankalexey@gmail.com>
2019-04-15 17:21:11 +0300

    Add EventBus.RabbitMQ

---
+++ src/EventBus.RabbitMQ/IPersistentConnection.cs
@@ -0,0 +1,15 @@
+﻿using RabbitMQ.Client;
+using System;
+
+namespace EventBus.RabbitMQ
+{
+    public interface IPersistentConnection
+        : IDisposable
+    {
+        bool IsConnected { get; }
+
+        bool TryConnect();
+
+        IModel CreateModel();
+    }
+}
